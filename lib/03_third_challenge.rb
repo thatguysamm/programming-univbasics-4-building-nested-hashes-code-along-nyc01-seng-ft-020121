c3e5c7b6603a64e5572b8cf39823ff258c37fb15
friendly-format-0074 <samuelagasi@gmail.com>
2021-01-19 21:42:21 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -2,12 +2,12 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        name: "Lord Montague", age: 53
+        name: "Lord Montague", age: "53"
         },
-      :matriarch => {name: "Lady Montague", age: 54
+      :matriarch => {name: "Lady Montague", age: "54"
         },
       :hero => {
-        name: "Romeo", age: 15, status: "alive"
+        name: "Romeo", age: "15", status: "alive"
         },
       :hero_friends => []
    },
